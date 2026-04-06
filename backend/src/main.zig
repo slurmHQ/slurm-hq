92bf63511f2410ca2e4515a8cbe004f294535545
Toni Harzendorf <toni.harzendorf@ufz.de>
2026-04-06 13:03:06 +0200

    add cors handler for development, run slurm_init after start

--- backend/src/main.zig
+++ backend/src/main.zig
@@ -3,8 +3,28 @@ const slurm = @import("slurm");
 const tk = @import("tokamak");
 const api = @import("api.zig");
 
+fn cors(children: []const tk.Route) tk.Route {
+    const H = struct {
+        fn handle(ctx: *tk.Context) anyerror!void {
+            ctx.res.header("Access-Control-Allow-Origin", "*");
+            ctx.res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+
+            // if (ctx.req.method == .OPTIONS) {
+            //     try ctx.res.send("", .{});
+            //     return;
+            // }
+
+            try ctx.next();
+        }
+    };
+
+    return .{ .handler = &H.handle, .children = children };
+}
+
 const routes: []const tk.Route = &.{
-    .group("/api", &.{.router(api)}),
+    cors(&.{
+        .group("/api", &.{.router(api)}),
+    }),
 };
 
 pub fn main() !void {
@@ -12,10 +32,12 @@ pub fn main() !void {
     const allocator = arena.allocator();
     defer _ = arena.deinit();
 
+    slurm.init(null);
+
     const server_opts: tk.ServerOptions = .{
         .listen = .{
             .hostname = "0.0.0.0",
-            .port = 8080,
+            .port = 8000,
         },
     };
     var server: tk.Server = try .init(allocator, routes, server_opts);
